07fd482ebb096297d6e9298b3230c2adb023eaab
IsabellaApple <isabellaapple111@gmail.com>
2019-10-24 11:40:44 +0800

    Updates

---
+++ precache-manifest.5a1eb15d1a352270e2b7520ba650c0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacc7f6f78c1bc4fbc2fdce3eadb5227",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "db294bf5d3906afcb701",
+    "url": "/hello-world/static/css/main.d8ec9770.chunk.css"
+  },
+  {
+    "revision": "4c1adbef7a1467a57f2e",
+    "url": "/hello-world/static/js/2.080eabd0.chunk.js"
+  },
+  {
+    "revision": "db294bf5d3906afcb701",
+    "url": "/hello-world/static/js/main.b05f6221.chunk.js"
+  },
+  {
+    "revision": "8068785796f70356ec1d",
+    "url": "/hello-world/static/js/runtime-main.0ef886a9.js"
+  },
+  {
+    "revision": "8933c1e0b1460e95dc81c356d5614b11",
+    "url": "/hello-world/static/media/example.8933c1e0.mp4"
+  }
+]);
